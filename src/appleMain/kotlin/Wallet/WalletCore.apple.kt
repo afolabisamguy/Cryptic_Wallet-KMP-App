d9d9425c6296b3782525b74da22f2ba4731b2f6d
Samguy1 <afolabiayoola001@gmail.com>
2024-10-17 06:21:16 +0100

    aksd

--- src/appleMain/kotlin/Wallet/WalletCore.apple.kt
+++
@@ -1,6 +0,0 @@
-package Wallet
-
-import com.softartdev.theme.pref.ThemeEnum
-
-actual fun setSystemBar(newTheme: ThemeEnum) {
-}
--- src/commonMain/kotlin/theme/Color.kt
+++
@@ -1,6 +0,0 @@
-package theme
-
-import androidx.compose.ui.graphics.Color
-
-val MyPrimaryColor = Color(0xff6D9B7B)
-val MyOnPrimaryColor = Color(0xffbeeaf7)
